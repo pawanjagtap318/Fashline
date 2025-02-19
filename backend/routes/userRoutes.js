94f0d6bd2ace1381807702dc2a13a02c038d80e5
pawanjagtap318 <pawanjagtap318@gmail.com>
2025-02-19 18:19:45 +0530

    post register request done

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// @route POST /api/users/register
+// @desc Register s new user
+// @access Public
+router.post("/register", async (req, res) => {
+    const {name, email, password} = req.body;
+    console.log(process.env.JWT_SECRET);
+
+    try {
+        // Registration Login
+        let user = await User.findOne({email});
+
+        if (user) return res.status(400).json({message: "User already exits."});
+
+        user = new User({name, email, password});
+        await user.save();
+
+        // Create JWT Payload
+        const payload = {user: {id: user._id, role: user.role}};
+
+        // Sign and Return the token along with the user data
+        jwt.sign(
+            payload, 
+            process.env.JWT_SECRET,
+            {expiresIn: "40h"},
+            (err, token) => {
+                if(err) throw err;
+
+                // Send the user and token in response
+                res.status(201).json({
+                    user: {
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                    },
+                    token,
+                });
+            }
+        );
+    } catch(error) {
+        console.log(error);
+        res.status(500).send("Server Error");
+    }
+});
+
+// 
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Welcome to Rabbit API!");
-})
+});
+
+// API routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on https://localhost:${PORT}`);
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
